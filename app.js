e91ab8e469f364c87bf6a011fd9f9735157a3330
FlavioFS <flaviofreitas.h@gmail.com>
2021-02-07 13:36:02 -0300

    Code cleaning

---
+++ app.js
@@ -0,0 +1,302 @@
+//////////////////////////////////////////////////
+// Tabs
+//////////////////////////////////////////////////
+const tabs = [
+    {
+        text: "The Developer",
+        icon: "mdi-code-braces"
+    },
+    {
+        text: "The Artist",
+        icon: "mdi-brush"
+    },
+    {
+        text: "About",
+        icon: "mdi-account-box"
+    }
+];
+
+
+//////////////////////////////////////////////////
+// Dev Projects
+//////////////////////////////////////////////////
+const devProjects = [
+    {
+        title: 'Battle transition shaders (old Pokémon Style)',
+        engine: {
+            name: 'Godot',
+            icon: 'img/godot.png',
+            website: 'https://godotengine.org/'
+        },
+        repo: 'https://github.com/flaviofs/godot-pokemon-transitions',
+        imgs: [
+            'https://i.postimg.cc/bvzMSHv3/preview1.gif',
+            'https://i.postimg.cc/SsRvDSC9/preview2.gif',
+            'https://i.postimg.cc/yNvGmggB/preview3.gif'
+        ]
+    },
+    {
+        title: 'Player Finite Multi-State Machine (2D Platformer) + Skin Selector ',
+        engine: {
+            name: 'Godot',
+            icon: 'img/godot.png',
+            website: 'https://godotengine.org/'
+        },
+        repo: 'https://github.com/FlavioFS/godot-platformer-state-machine',
+        imgs: [
+            'https://i.postimg.cc/pL97fXQn/preview.gif',
+            'https://i.postimg.cc/kGsrp1D6/extra.gif'
+        ]
+    },
+    {
+        title: 'Multi-state enemy behavior (Smashing Stone)',
+        engine: {
+            name: 'Godot',
+            icon: 'img/godot.png',
+            website: 'https://godotengine.org/'
+        },
+        repo: 'https://github.com/FlavioFS/godot-mad-stone',
+        imgs: [
+            'https://i.postimg.cc/9Mc7h0J4/preview.gif'
+        ]
+    },
+    {
+        title: 'Client data analytics (2 separated repos: client & server)',
+        engine: {
+            name: 'Cocos Creator',
+            icon: 'img/cocos.png',
+            website: 'https://www.cocos.com/en/creator'
+        },
+        repo: 'https://github.com/FlavioFS/slot-test-client',
+        imgs: [
+            'https://i.postimg.cc/63NhrYJX/slotclientdemo.gif',
+            'https://i.postimg.cc/cHZzcfPV/slotserverdemo.gif'
+        ]
+    },
+    {
+        title: 'Complete Android game from scratch',
+        engine: {
+            name: 'Unity',
+            icon: 'img/unity.png',
+            website: 'https://unity.com/'
+        },
+        store: {
+            icon: 'img/google-play.png',
+            link: 'https://play.google.com/store/apps/details?id=com.MeronSoda.DontDrop'
+        },
+        imgs: [
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/497/large/flavio-sousa-grafico-de-recursos2.jpg?1530020062',
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/777/large/flavio-sousa-1.jpg?1530021096',
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/519/large/flavio-sousa-2.jpg?1530020190',
+            'https://cdna.artstation.com/p/assets/images/images/011/523/520/large/flavio-sousa-3.jpg?1530020194',
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/523/large/flavio-sousa-4.jpg?1530020198',
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/599/large/flavio-sousa-5.jpg?1530020468',
+            'https://cdna.artstation.com/p/assets/images/images/011/523/624/large/flavio-sousa-6.jpg?1530020563',
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/635/large/flavio-sousa-7.jpg?1530020591',
+            'https://cdnb.artstation.com/p/assets/images/images/011/523/683/large/flavio-sousa-8.jpg?1530020746'
+        ]
+    },
+    {
+        title: '2D Sea shader - 1 Quad',
+        engine: {
+            name: 'Unity',
+            icon: 'img/unity.png',
+            website: 'https://unity.com/'
+        },
+        imgs: [
+            'https://i.postimg.cc/nzcxP9JQ/water-shader1.gif',
+            'https://i.postimg.cc/vZsZvBc5/water-shader2.gif'
+        ]
+    },
+    {
+        title: '5 Minute Dungeon mod (Tabletop Simulator)',
+        engine: {
+            name: 'Lua',
+            icon: 'img/lua.png',
+            website: 'https://www.lua.org/'
+        },
+        store: {
+            icon: 'img/steam.svg',
+            link: 'https://steamcommunity.com/sharedfiles/filedetails/?id=1934822334'
+        },
+        repo: 'https://github.com/FlavioFS/5-minute-dungeon-tts-mod',
+        imgs: [
+            'https://i.postimg.cc/GpyBYYST/sample3-next.gif',
+            'https://i.postimg.cc/BbMtsCtk/sample1-boss.gif',
+            'https://i.postimg.cc/DZQ8phh2/sample2-setup.gif',
+            'https://i.postimg.cc/GpyBYYST/sample3-next.gif',
+            'https://i.postimg.cc/4dRY1drL/sample4-draw.gif'
+        ]
+    }
+];
+
+
+//////////////////////////////////////////////////
+// Art Projects
+//////////////////////////////////////////////////
+const artProjects = [
+    {
+        title: 'Goblin head sculpt',
+        imgs: [
+            'https://i.postimg.cc/Y0L94h8Z/output.gif',
+            'https://i.postimg.cc/3NT572Yx/0001.png',
+            'https://i.postimg.cc/0jYvJccS/0011.png',
+            'https://i.postimg.cc/ZKm4hz5x/0033.png',
+            'https://i.postimg.cc/DycFvpWk/0051.png',
+            'https://i.postimg.cc/135Sf06g/0077.png',
+            'https://i.postimg.cc/CxPgDZHc/0094.png',
+            'https://i.postimg.cc/59LbG2bt/0135.png',
+            'https://i.postimg.cc/W4dP725T/0159.png',
+            'https://i.postimg.cc/W4xcWQyr/0172.png'
+        ]
+    },
+    {
+        title: 'Lumiel',
+        imgs: [
+            'https://cdnb.artstation.com/p/assets/images/images/033/586/747/large/flavio-freitas-flaf-lumiel.jpg?1610025593',
+            'https://cdnb.artstation.com/p/assets/images/images/033/586/753/large/flavio-freitas-flaf-lumiel2.jpg?1610025613'
+        ]
+    },
+    {
+        title: 'Pink Ribbon',
+        imgs: [
+            'https://cdna.artstation.com/p/assets/images/images/033/706/738/large/flavio-freitas-flaf-pink-ribbon.jpg?1610372052'
+        ]
+    }
+];
+
+
+//////////////////////////////////////////////////
+// About
+//////////////////////////////////////////////////
+const about = {
+    software: [
+        {
+            name: "Krita",
+            icon: "img/krita.png"
+        },
+        {
+            name: "Photoshop",
+            icon: "img/photoshop.png"
+        },
+        {
+            name: "Unity",
+            icon: "img/unity.png"
+        },
+        {
+            name: "Godot",
+            icon: "img/godot.png"
+        },
+        {
+            name: "Illustrator",
+            icon: "img/illustrator.png"
+        },
+        {
+            name: "Aseprite",
+            icon: "img/aseprite.png"
+        },
+        {
+            name: "Pyxel Edit",
+            icon: "img/pyxeledit.png"
+        }
+    ]
+};
+
+
+//////////////////////////////////////////////////
+// Social
+//////////////////////////////////////////////////
+const social = [
+    {
+        name: 'GitHub',
+        icon: 'img/github.svg',
+        link: 'https://www.github.com/FlavioFS/'
+    },
+    {
+        name: 'ArtStation',
+        icon: 'img/artstation.svg',
+        link: 'https://www.artstation.com/flaf'
+    },
+    {
+        name: 'DeviantArt',
+        icon: 'img/deviantart.svg',
+        link: 'https://www.deviantart.com/flafdraws'
+    },
+    {
+        name: 'Instagram',
+        icon: 'img/instagram.svg',
+        link: 'https://www.instagram.com/flafdraws/'
+    }
+];
+
+
+//////////////////////////////////////////////////
+// Vue
+//////////////////////////////////////////////////
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify({
+        theme: {
+            dark: true
+        }
+    }),
+    computed: {
+        isMobile: () => {
+            return window.isMobile();
+        }
+    },
+    methods: {
+        onArtProjectClick: function (projectIndex) {
+            try {
+                this.currentArtProject = this.artProjectsReverse[projectIndex];
+                this.isArtDialogActive = true;
+            } catch (error) {
+                this.isArtDialogActive = false;
+            } finally {
+                this.currentArtIndex = 0;
+            }
+        },
+        incrementArtIndex: function () {
+            if (this.currentArtProject.imgs !== undefined && this.currentArtIndex < this.currentArtProject.imgs.length - 1) {
+                this.currentArtIndex++;
+            }
+        },
+        decrementArtIndex: function () {
+            if (this.currentArtProject.imgs !== undefined && this.currentArtIndex > 0) {
+                this.currentArtIndex--;
+            }
+        },
+        saveTab: function (tabIndex) {
+            localStorage.setItem("tabIndex", tabIndex);
+        }
+    },
+    computed: {
+        artProjectsReverse() {
+            return this.artProjects.slice().reverse();
+        }
+    },
+    mounted() {
+        try {
+            const tabIndex = parseInt(localStorage.getItem("tabIndex"));
+            if (tabIndex >= 0 && tabIndex < this.tabs.length) {
+                this.tabIndex = tabIndex;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    data() {
+        return {
+            isReady: false,
+            tabIndex: 0,
+            isArtDialogActive: false,
+            currentArtProject: undefined,
+            currentArtIndex: 0,
+            tabs: tabs,
+            devProjects: devProjects,
+            artProjects: artProjects,
+            about: about,
+            social: social
+        }
+    }
+});
